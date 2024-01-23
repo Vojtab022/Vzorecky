a7bf7f967aaf15afb5d42562dc1cbca3a3ce44c5
Vojtěch Bezděk <Vojtech.Bezdek@souepl.local>
2024-01-23 13:12:08 +0100

    2 hodina

---
+++ Všechny.py
@@ -0,0 +1,67 @@
+#Uvítání+nabídka možností
+print ('Vítejte v aplikaci pro výpočet matematických vzorečků')
+print("1.Obvod obdelníku")
+print("2.Obsah obdelníku")
+print ("3.Objem kvádru")
+print("4.Obsah kruhu")
+vyber = input("Vyberte si co budeme dnes počítat zadáním čísla 1-4: ")
+if vyber == "1":
+    #Deklarace proměných
+    a = input ('Zadejte délku strany a: ')
+    b = input ('Zadejte délku strany b: ')
+    #Přetypování na int
+    a = int(a)
+    b = int(b)
+    #Kontrola záporného čísla
+    if a>0 and b>0:
+        print('Výsledek je: ')
+        print(2*a+2*b)
+    #Pokud není splněná podmínka tak napíše výsledek
+    else:
+        print('Co pak to děláš? Ty troubo zadal jsi zápor')
+elif vyber == "2":
+    #Deklarace proměných
+    a = input ('Zadejte délku strany a: ')
+    b = input ('Zadejte délku strany b: ')
+    #Přetypování na int
+    a = int(a)
+    b = int(b)
+    #Kontrola záporného čísla
+    if a>0 and b>0:
+        print('Výsledek je: ')
+        print(a*b)
+    #Pokud není splněná podmínka tak napíše výsledek
+    else:
+        print('Co pak to děláš? Ty troubo zadal jsi zápor')
+elif vyber == "3":
+    #Deklarace proměných
+    a = input ('Zadejte délku strany a: ')
+    b = input ('Zadejte délku strany b: ')
+    c = input ('Zadejte délku strany c: ')
+    #Přetypování na int
+    a = int(a)
+    b = int(b)
+    c = int(c)
+    #Kontrola záporného čísla
+    if a>0 and b>0 and c>0:
+        print('Výsledek je: ')
+        print(a*b*c)
+    #Pokud není splněná podmínka tak napíše výsledek
+    else:
+        print('Co pak to děláš? Ty troubo zadal jsi zápor')
+elif vyber =="4":
+    #Deklarace proměných
+    r = input ('Zadejte poloměr kruhu r: ')
+    #Vložení math
+    import math
+    #Přetypování na int
+    r = int(r)
+    #Kontrola záporného čísla
+    if r>0:
+        print('Výsledek je: ')
+        print(math.pi*r**2)
+    #Pokud není splněná podmínka tak napíše výsledek
+    else:
+        print('Co pak to děláš? Ty troubo zadal jsi zápor')
+else:
+    print ("Nevybral sis žádnou z možností")
